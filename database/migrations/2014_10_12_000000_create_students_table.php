9ecc2d32c003858ffcc51c1c40700ad0497e616c
Hector Rojas Barcenas <hectorrojasbarcenas@MacBook-Air-de-Hector.local>
2023-01-23 18:26:57 -0600

    Include database migrations

--- database/migrations/2014_10_12_000000_create_students_table.php
+++ database/migrations/2014_10_12_000000_create_students_table.php
@@ -13,13 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('students', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->bigInteger('student_id');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('students');
     }
 };
