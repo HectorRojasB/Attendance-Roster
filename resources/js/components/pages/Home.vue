d616e747d6f21e2722e6abcf4bff184193312a96
Hector Rojas Barcenas <hectorrojasbarcenas@MacBook-Air-de-Hector.local>
2023-01-24 10:13:19 -0600

    Include minor styles and markup updates

--- resources/js/components/pages/Home.vue
+++ resources/js/components/pages/Home.vue
@@ -3,6 +3,9 @@ import { onMounted } from "vue";
 import { store } from "../../utils/store.js";
 import { getCourses, getStudents } from "../../utils/apiRoutes";
 
+const dateOptions = { weekday: "long", year: "numeric", month: "long", day: "numeric" };
+const currentDate = new Date().toLocaleDateString("en-US", dateOptions);
+
 onMounted(() => {
     getCourses();
     getStudents();
@@ -12,15 +15,19 @@ onMounted(() => {
     <div class="container home">
         <div class="row d-flex justify-content-center text-center">
             <div class="col-md-5 col-sm-12 main-container">
-                <h3>Attendance</h3>
-                <p>Choose a course</p>
-                <div class="text-center select-course-container">
-                    <select class="form-select" aria-label="Default select example">
-                        <option disabled selected>Select a course</option>
-                        <option v-for="course in store.courses.data" :key="course.id">
-                            {{ course.name }}
-                        </option>
-                    </select>
+                <h3 class="fw-bold">Attendance</h3>
+                <h5>{{ currentDate }}</h5>
+
+                <div class="text-start course-container">
+                    <p class="label">Choose a course</p>
+                    <div class="text-center select-course-container">
+                        <select class="form-select" aria-label="Default select example">
+                            <option disabled selected>Select a course</option>
+                            <option v-for="course in store.courses.data" :key="course.id">
+                                {{ course.name }}
+                            </option>
+                        </select>
+                    </div>
                 </div>
 
                 <div class="students-container">
